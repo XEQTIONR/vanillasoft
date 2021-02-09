ab3b9aeb64eab9ea65cca5c48455441881c95663
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2021-02-09 17:31:47 -0500

    Adding list emails route and controller function

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Jobs\SendEmailJob;
 
+use App\Models\Email;
 use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
     //
+
+    public function list(Request  $request) {
+        $validated = $request->validate([
+            'api_key' => 'required'
+        ]);
+
+        return Email::with('attachments')->get();
+    }
+
     public function send(Request $request) {
 
         $validated = $request->validate([
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/send', EmailController::class.'@send');
+
+Route::get('/list', EmailController::class.'@list');
